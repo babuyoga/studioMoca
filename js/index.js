c58a10b3613dd8c7163b69b75c6245143d196442
babuyoga <56405793+babuyoga@users.noreply.github.com>
2021-08-24 22:30:53 +0530

    Make final changes before launch

--- js/index.js
+++ js/index.js
@@ -13,38 +13,8 @@ function loadnew(video_num) {
 document.getElementById("home_player").src = "resources/videos/"+video_num+".webm";
 }
 
-// $("#work").hover(loadnew());
-function myFunction(x) {
-  if (x.matches) { // If media query matches
 
-window.addEventListener("deviceorientation", handleOrientation, true);
-
-
-function handleOrientation(event) {
-  var absolute = event.absolute;
-  var alpha    = event.alpha;
-  var beta     = event.beta;
-  var gamma    = event.gamma;
-
-console.log(absolute);
-console.log(alpha); //0to360
-console.log(beta); //-180to180
-console.log(gamma);//-90to90
-
-
-if(gamma > 0){
- $("#bgvid").css('left', ((gamma/90)*100));
-  // Do stuff with the new orientation data
-}else {
-  $("#bgvid").css('left', (-1*(gamma/90)*100));
+function onbodyload(){
+    document.getElementById("bg_player").src = "resources/videos/"+7+".webm";
+    document.getElementById("home_player").src = "resources/videos/"+1+".webm";
 }
-
-
-  } else {
-
-  }
-}
-
-var x = window.matchMedia("(max-width: 1000px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
